0ea9b13b194a8d1391dfad143e1c5cf0faa066db
ljorgens614 <77366860+ljorgens614@users.noreply.github.com>
2023-05-22 15:58:19 +0000

    Completed challenges 1-6

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,18 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT *
+FROM Customers
+WHERE 
+  FirstName = 'Norby'
+  OR LastName = 'Norby';
+
+SELECT *
+FROM Reservations
+WHERE CustomerID = 64;
+
+
+DELETE FROM Reservations
+WHERE CustomerID = 64
+  AND ReservationID = 2000;
