5642fc0395288be684b04e23ac727e49abc163fd
ljorgens614 <77366860+ljorgens614@users.noreply.github.com>
2023-06-07 13:19:51 +0000

    Completed Chapter 10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,42 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+INSERT INTO Orders (CustomerID, OrderDate)
+VALUES (70, '2022-09-20 14:00:00')
+
+SELECT *
+FROM Orders
+WHERE CustomerID = 70 AND OrderID = 1001
+
+INSERT INTO OrdersDishes (OrderID, )
+  SELECT OrderID
+  FROM Orders
+  WHERE CustomerID = 70 AND OrderID = 1001;
+  
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES 
+  (1001, 7),
+  (1001, 20)
+
+UPDATE OrdersDishes
+SET DishID = 4
+WHERE OrdersDishesID = 4022
+
+SELECT *
+FROM OrdersDishes
+WHERE OrderID = 1001
+
+SELECT OrderID,
+  SUM(DishID) AS 'TotalCost'
+FROM OrdersDishes
+WHERE OrderID = 1001
+
+SELECT DishID,
+  Name,
+  Price
+FROM Dishes
+WHERE
+  Name = 'Mini Cheeseburgers'
+  OR Name = 'House Salad'
+  OR Name = 'Tropical Blue Smoothie'
+
